b0983379753efba36920797e396324d46deb7ae4
RajeshKanna <shanmugam10052966@gmail.com>
2026-04-01 16:40:30 +0530

    final fix: auth hashing stable

--- app/auth.py
+++ app/auth.py
@@ -10,11 +10,14 @@ ACCESS_TOKEN_EXPIRE_HOURS = 24
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
 def hash_password(password: str) -> str:
+    password = password.encode("utf-8")[:72].decode("utf-8")
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    plain_password = plain_password.encode("utf-8")[:72].decode("utf-8")
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
